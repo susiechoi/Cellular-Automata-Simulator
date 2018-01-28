0b54623a6a76888d42fd56663ce80c2122b5fd37
Susie Choi <sc477@duke.edu>
2018-01-28 18:20:43 -0500

    Updated Grid class

--- src/cellsociety_team17/Grid.java
+++ src/cellsociety_team17/Grid.java
@@ -1,9 +1,36 @@
 package cellsociety_team17;
 
+import java.util.ArrayList;
+
 public class Grid {
 
-	public Grid() {
-		// TODO Auto-generated constructor stub
+	int mySimulationType;
+	int myWidth;
+	int myHeight;
+	Cell[][] myCells; 
+
+	public Grid(int width, int height) {
+		this(1, 400, 400, new ArrayList<Cell>()); 
+	}
+
+	public Grid(int simType, int width, int height, ArrayList<Cell> activePts) {
+		mySimulationType = simType;
+		myWidth = width;
+		myHeight = height;
+		myCells = new Cell[myWidth][myHeight]; 
+		// TODO use active points arraylist
+	}
+	
+	private Group getGroup() {
+
+	}
+	
+	private int getWidth() {
+		return myWidth;
+	}
+	
+	private int getHeight() {
+		return myHeight; 
 	}
 
 }
