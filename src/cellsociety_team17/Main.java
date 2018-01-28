a9d0e11e82fa2066c09ed75b22cdbc096080f060
Collin Brown <cdb55@duke.edu>
2018-01-28 15:51:07 -0500

    created Main class

---
+++ src/cellsociety_team17/Main.java
@@ -0,0 +1,14 @@
+package cellsociety_team17;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
