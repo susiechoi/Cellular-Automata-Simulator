59ff3b72656ae98f982b9c062530d13e1c2e61e9
Collin Brown <cdb55@duke.edu>
2018-01-28 16:18:44 -0500

    added empty planned methods

--- src/cellsociety_team17/Main.java
+++ src/cellsociety_team17/Main.java
@@ -1,18 +1,55 @@
 package cellsociety_team17;
 
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	private Stage myPrimaryStage;
+	private Scene myScene;
+	//private Grid myGrid;
+	private int mySimulationType;
+	//private ArrayList<cell> activeCells;
+	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		
-		
+			//TODO: Complete Start
 	}
 	
+	/**
+	 * 
+	 * @param timeElapsed
+	 * @return
+	 */
+	private Object Step(Double timeElapsed) {
+		//TODO: Complete Step
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param e
+	 */
+	private void handleMouseInput(MouseEvent e) {
+		//TODO: Complete handleMouseInput
+	}
+	
+	private void startSimulation(grid G) {
+		//TODO: completeStartSimulation
+	}
+	
+	private grid readInput(File f) {
+		//TODO: completeReadInput
+	}
 }
