bd359fd99d77b58c8ec122791b27f32c8b97e9e9
Collin Brown <cdb55@duke.edu>
2018-01-28 15:56:45 -0500

    created Main.java

--- src/cellsociety_team17/Main.java
+++ src/cellsociety_team17/Main.java
@@ -5,9 +5,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
