43b76eae64457205fb57b0698448d6e0f543e01c
Judith Sanchez Sorian <js640@duke.edu>
2018-01-28 22:05:34 -0500

    created cell superclass

--- src/cellsociety_team17/Cell.java
+++ src/cellsociety_team17/Cell.java
@@ -1,5 +1,27 @@
 package cellsociety_team17;
 
-public class Cell {
+import java.util.ArrayList;
 
+public abstract class Cell {
+
+	private int myState; 
+	private int myType;
+
+	public int getMyState() {
+		return myState;
+	}
+	
+	public void setMyState(int state) {
+		myState= state;
+	}
+	
+	public ArrayList<Cell> getNeighbors() {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+		// This ArrayList needs to be a different size for corner, edge, and inner cells. 
+		// So each cell needs to have an indicator for what type it is
+		return neighbors;
+		
+	}
+	
+	abstract void update();
 }
