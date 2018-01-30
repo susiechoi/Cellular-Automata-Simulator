2149df593b535567dc7a9ae42932d61de34ce977
Susie Choi <sc477@duke.edu>
2018-01-30 16:30:04 -0500

    removed default threshold float: will come from XML

--- src/cellsociety_team17/SegregationCell.java
+++ src/cellsociety_team17/SegregationCell.java
@@ -3,7 +3,7 @@ package cellsociety_team17;
 public class SegregationCell extends Cell {
 
 	// threshold t of satisfaction w/ segregation
-	public static final float DEFAULT_THRESHOLD = 0.5;
+//	public static final float DEFAULT_THRESHOLD = 0.5;
 	private float myThreshold;
 
 	public SegregationCell(int row, int col, int startState) {
