57a702ebb636c763e7a81fb336998c574c24679b
Collin Brown <cdb55@duke.edu>
2018-02-01 17:01:49 -0500

    completed establishScene method

--- src/cellsociety_team17/SimulationView.java
+++ src/cellsociety_team17/SimulationView.java
@@ -3,8 +3,10 @@ package cellsociety_team17;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 
 public class SimulationView {
+	private static final double MIN_WIDTH = 100;
 	private double myHeight;
 	private double myWidth;
 	private double myHeaderHeight;
@@ -22,6 +24,16 @@ public class SimulationView {
 		myGrid = g;
 		myHeaderHeight = 50;
 		myHeight = myHeaderHeight + myGrid.getHeightInPixels() + myControlsContainerHeight;
+		
+		if(myGrid.getWidthInPixels() > MIN_WIDTH) {
+			myWidth = myGrid.getWidthInPixels();
+		} else {
+			myWidth = MIN_WIDTH;
+		}
+		setUpHeader();
+		setUpGridContainer();
+		setUpControls();
+		establishScene();
 	}
 	
 	public Scene getScene() {
@@ -29,14 +41,15 @@ public class SimulationView {
 	}
 	
 	public void establishScene() {
-		myScene = new Scene(myRoot);
+		myScene = new Scene(myRoot, myWidth, myHeight);
 		myRoot.getChildren().addAll(myHeader);
 		myRoot.getChildren().addAll(myGridContainer);
 		myRoot.getChildren().addAll(myControlsContainer);
 	}
 	
 	private void setUpHeader() {
-		
+		myHeader = new Group();
+		myHeader.getChildren().add(new Rectangle(myHeaderWidth, myHeaderHeight));
 	}
 	private void setUpControls() {
 		
