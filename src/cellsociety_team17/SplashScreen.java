e12e4ce3d316b5f9efbe6764815fa6a66c7c01e8
Susie Choi <sc477@duke.edu>
2018-02-01 16:55:14 -0500

    Added SplashScreen class

---
+++ src/cellsociety_team17/SplashScreen.java
@@ -0,0 +1,18 @@
+package cellsociety_team17;
+
+public class SplashScreen {
+	
+	private ArrayList<Rectangle> mySimulationOptions; 
+
+	public SplashScreen() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private String getUserSelection() {
+	}
+	
+	private void handleMouseInput(double xPos, double yPos) {
+		
+	}
+
+}
