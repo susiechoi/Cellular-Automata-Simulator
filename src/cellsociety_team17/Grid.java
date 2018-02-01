76be1a0bc7c2b513746f7da4bcff21b3119514f6
Susie Choi <sc477@duke.edu>
2018-02-01 10:21:08 -0500

    segregation cell

--- src/cellsociety_team17/Grid.java
+++ src/cellsociety_team17/Grid.java
@@ -1,5 +1,6 @@
 package cellsociety_team17;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
 import javafx.scene.Group;
@@ -9,25 +10,14 @@ public class Grid {
 
 	private int myWidth;
 	private int myHeight;
-	private int mySimulationType;
 	private Cell[][] myCells; 
 	private Group myGroup;
 
-	public Grid(int width, int height) {
-		this(1, 400, 400, new ArrayList<Cell>()); 
-	}
-
-	public Grid(int width, int height, int simulationType, ArrayList<Cell> activeCells) {
+	public Grid(int width, int height, ArrayList<Cell> cells) {
 		myWidth = width;
 		myHeight = height;
 		myCells = new Cell[myHeight][myWidth]; 
-		mySimulationType = simulationType; 
-		for (int row=0; row<myHeight; row++) {
-			for (int col=0; col<myWidth; col++) {
-				myCells[row][col] = new Cell(row, col, 0);
-			}
-		}
-		for (Cell cell : activeCells) {
+		for (Cell cell : cells) {
 			myCells[cell.myRow][cell.myCol] = cell; 
 		}
 		for (Cell cell : cells) {
@@ -41,13 +31,13 @@ public class Grid {
 		cell.setNeighbors(inBoundsNeighbors);
 	}
 	
-	// TODO account for each different Simulation type in cell 
 	private ArrayList<Cell> findInBoundsNeighbors(Cell cell) {
 		ArrayList<Cell> neighbors = new ArrayList<Cell>();
-		if (cell.myRow > 0) neighbors.add(myCells[cell.myRow-1]);
-		if (cell.myRow < myHeight-1) neighbors.add(myCells[cell.myRow+1]);
-		if (cell.myCol > 0) neighbors.add(myCells[cell.myCol-1]);
-		if (cell.myCol < myWidth-1) neighbors.add(myCells[cell.myCol+1]);
+		if (cell.myRow > 0) neighbors.add(myCells[cell.myRow-1][cell.myCol]);
+		if (cell.myRow < myHeight-1) neighbors.add(myCells[cell.myRow+1][cell.myCol]);
+		if (cell.myCol > 0) neighbors.add(myCells[cell.myRow][cell.myCol-1]);
+		if (cell.myCol < myWidth-1) neighbors.add(myCells[cell.myRow][cell.myCol+1]);
+		return neighbors; 
 	}
 
 	public void updateCells(ArrayList<Cell> activeCells) {
---
+++ src/cellsociety_team17/SegregationCell.java
@@ -0,0 +1,91 @@
+package cellsociety_team17;
+
+import java.util.LinkedList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javafx.scene.control.Cell;
+import javafx.scene.paint.Color;
+
+public class SegregationCell extends Cell {
+
+	public static final Color[] STATE_COLORS = {Color.WHITE, Color.BLUE, Color.RED}; 
+
+	// threshold t represents satisfaction with segregation 
+	// TODO in Grid class, add sthg so that if it's the segregation simulation, also the immediate diagonal cells are added
+	private float myThreshold;
+
+	public SegregationCell(int row, int col, int startState) {
+		super(row, col, startState);
+	}
+
+	public SegregationCell(int row, int col, int startState, float threshold) {
+		super(row, col, startState);
+		myThreshold = threshold; 
+	}
+
+	public void setMyState(int state) {
+		myState = state;
+	}
+
+	public int getMyState() {
+		return myState;
+	}
+
+	public void setNeighbors(ArrayList<Cell> neighbors) {
+		myNeighbors = neighbors; 
+	}
+
+	public ArrayList<Cell> getNeighbors(){
+		return myNeighbors;
+	}
+
+	public void updateState() {
+		if (this.needToMove()) { 
+			this.moveToEmptySpace();
+		}
+	}
+
+	private boolean needToMove() {
+		int neighborsLikeMe = 0;
+		int nonEmptyNeighbors = -1; 
+		for (Cell neighbor : myNeighbors) {
+			if (neighbor.myState != 0) {
+				if (neighbor.myState = myState) neighborsLikeMe++; 
+				nonEmptyNeighbors++; 
+			}
+		}
+		return ((neighborsLikeMe / nonEmptyNeighbors) < myThreshold);
+	}
+
+	private void moveToEmptySpace() {
+		CopyOnWriteArrayList<Cell> possEmptySpots = new CopyOnWriteArrayList<Cell>();
+
+		for (Cell neighbor : myNeighbors) {
+			if (neighbor.myState == 0) {
+				neighbor.myState = myState;
+				myState = 0; 
+				updateColor(this); 
+				updateColor(neighbor); 
+				return;
+			} 
+			possEmptySpots.add(neighbor);
+		}
+		for (Cell possSpot : possEmptySpots) {
+			if (possSpot.myState == 0) {
+				possSpot.myState = myState;
+				myState = 0; 
+				updateColor(this); 
+				updateColor(neighbor); 
+				return;
+			} 
+			possEmptySpots.addAll(possSpot.myNeighbors);
+		}
+		throw new Exception("Threshold unsatisfied, but no empty spots to move to!");
+	}
+
+	private void updateColor(Cell cell) {
+		cell.myRectangle.setFill(STATE_COLORS[cell.myState]);
+	}
+
+
+}
