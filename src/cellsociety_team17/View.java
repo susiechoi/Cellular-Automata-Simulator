69f2a04f2a0ae18f82883a4b17982558b21bf90a
Collin Brown <cdb55@duke.edu>
2018-02-01 16:30:05 -0500

    created view class

---
+++ src/cellsociety_team17/View.java
@@ -0,0 +1,13 @@
+package cellsociety_team17;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class View extends Scene{
+
+	public View(Parent p, double height, double width) {
+		super(p, height, width);
+		// TODO Auto-generated constructor stub
+	}
+
+}
