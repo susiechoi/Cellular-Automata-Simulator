4c0bddfeeffa324fad249a4bf85215c750ab97a6
Judith Sanchez Sorian <js640@duke.edu>
2018-02-11 17:41:58 -0500

    basic graphing on seperate stage

--- src/cellsociety_team17/Graphing.java
+++ src/cellsociety_team17/Graphing.java
@@ -13,17 +13,19 @@ import java.util.List;
 public class Graphing {
 	private Group myRoot;
 	private Scene graphScene;
-	private NumberAxis yaxis= new NumberAxis(0, 50, 1);
-	private NumberAxis xaxis= new NumberAxis(0, 50, 1);
+	private NumberAxis yaxis= new NumberAxis(0, 25, 1);
+	private NumberAxis xaxis= new NumberAxis(0, 25, 1);
 	private LineChart<Number, Number> myChart;
 	private Series<Number, Number> typeOne = new Series<Number, Number>();
-	//private Series<Number, Number> typeTwo = new Series<Number, Number>();
-	//private Series<Number, Number> typeThree = new Series<Number, Number>();
+	private Series<Number, Number> typeTwo = new Series<Number, Number>();
+	private Series<Number, Number> typeThree = new Series<Number, Number>();
 	private int count;
 	
 	public Graphing() {
 		myChart = new LineChart<Number, Number>(xaxis, yaxis);
-		//myChart.setMaxHeight(200);
+		myChart.getData().add(typeOne);
+		myChart.getData().add(typeTwo);
+		myChart.getData().add(typeThree);
 		myRoot= new Group();
 		newScene();
 		count=0;
@@ -32,7 +34,6 @@ public class Graphing {
 	public void newScene() {
 		graphScene = new Scene(myRoot);
 		myRoot.getChildren().add(myChart);
-		myChart.getData().add(typeOne);
 	}
 
 	private int[] countCells(List<Cell> allCells) {
@@ -47,13 +48,12 @@ public class Graphing {
 		int[] numCells = countCells(allCells);
 		XYChart.Data<Number, Number> data1 = new XYChart.Data<Number, Number>(count, numCells[0]);
 		typeOne.getData().add(data1);
-		//Data<Number, Number> data2 = new Data<Number, Number>(2, numCells[1]);
-		//typeTwo.getData().add(data2);
-		//Data<Number, Number> data3 = new Data<Number, Number>(2, numCells[2]);
-		//typeThree.getData().add(data3);
+		XYChart.Data<Number, Number> data2 = new XYChart.Data<Number, Number>(count, numCells[1]);
+		typeTwo.getData().add(data2);
+		XYChart.Data<Number, Number> data3 = new XYChart.Data<Number, Number>(count, numCells[2]);
+		typeThree.getData().add(data3);
 		
-		//myChart.getData().add(typeTwo);
-		//myChart.getData().add(typeThree);
+		
 		count++;
 
 	}
